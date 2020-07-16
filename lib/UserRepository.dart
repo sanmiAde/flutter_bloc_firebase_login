dd6fe6585bfad5729f39f1af85c90bca7b5df8dd
oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2020-07-16 13:11:55 +0100

    initial commit

---
+++ lib/UserRepository.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  UserRepository(FirebaseAuth firebaseAuth) : _firebaseAuth = firebaseAuth;
+
+  Future<void> signInWithCredentials(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signUp({String email, String password}) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    return await _firebaseAuth.signOut();
+  }
+
+  Future<bool> isSignedIn() async {
+    final currentUser = await _firebaseAuth.currentUser();
+    return currentUser != null;
+  }
+
+  Future<String> getUser() async {
+    return (await _firebaseAuth.currentUser()).email;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'SimpleBlocObserver.dart';
+import 'UserRepository.dart';
+import 'authentication/authentication_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
+  final UserRepository userRepository = UserRepository(FirebaseAuth.instance);
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(userRepository: userRepository)
+        ..add(AuthenticationStarted()),
+      child: App(userRepository: userRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is AuthenticationInitial) {
+            return SplashScreen();
+          }
+
+          if (state is AuthenticationSuccess) {
+            return HomeScreen(name: state.displayName);
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Splash Screen')),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final String name;
+
+  HomeScreen({Key key, @required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context).add(
+                AuthenticationLoggedOut(),
+              );
+            },
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(child: Text('Welcome $name!')),
+        ],
+      ),
+    );
+  }
+}
